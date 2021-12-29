ab80096163993b439dca65769ac56b43ef4498b1
Asheer Rizvi <asheer.vil@gmail.com>
2021-12-29 11:28:43 +0530

    Formatted code with 4 spaces for indentation

--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -15,128 +15,131 @@ import { Container } from "@material-ui/core";
 let { config } = require("../../config");
 
 const useStyles = makeStyles({
-  totalDepositContainer: {
-    padding: "10px",
-  },
-  cumulativeDataRow: {
-    display: "grid",
-    gridTemplateColumns: "repeat(4, minmax(280px, 1fr))",
-  },
-  chainDataRow: {
-    display: "grid",
-    gridTemplateColumns: "repeat(3, minmax(280px, 1fr))",
-  },
-  graphContainer: {
-    padding: "15px",
-  },
-  graphComponentRow: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-  },
+    totalDepositContainer: {
+        padding: "10px",
+    },
+    cumulativeDataRow: {
+        display: "grid",
+        gridTemplateColumns: "repeat(4, minmax(280px, 1fr))",
+    },
+    chainDataRow: {
+        display: "grid",
+        gridTemplateColumns: "repeat(3, minmax(280px, 1fr))",
+    },
+    graphContainer: {
+        padding: "15px",
+    },
+    graphComponentRow: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+    },
 });
 
 export default function Home(props) {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  let durationStyle = {
-    backgroundImage: "none",
-    color: "#615CCD",
-    border: "2px solid #111",
-  };
+    let durationStyle = {
+        backgroundImage: "none",
+        color: "#615CCD",
+        border: "2px solid #111",
+    };
 
-  let supportedChainIds = [];
-  if (config.supportedChainArrray && config.supportedChainArrray.length > 0) {
-    config.supportedChainArrray.forEach((chain) =>
-      supportedChainIds.push(chain.chainId)
-    );
-  }
+    let supportedChainIds = [];
+    if (config.supportedChainArrray && config.supportedChainArrray.length > 0) {
+        config.supportedChainArrray.forEach((chain) =>
+            supportedChainIds.push(chain.chainId)
+        );
+    }
 
-  return (
-    <Container maxWidth="xl">
-      <div className={classes.totalDepositContainer}>
-        <div className={classes.cumulativeDataRow}>
-          <TotalDepositWithDuration
-            chainIds={supportedChainIds}
-            days={1}
-            title="24 hr Volume"
-            showComparision={true}
-            lightBackground={true}
-            style={durationStyle}
-          />
-          <CumulativeDeposit
-            title="Total Volume"
-            chainIds={supportedChainIds}
-            style={durationStyle}
-          />
-          <UniqueWalletCount
-            title="Unique User Count"
-            chainIds={supportedChainIds}
-            style={durationStyle}
-          />
-          <AverageTransferTime
-            style={durationStyle}
-            fromChainId={supportedChainIds[0]}
-            toChainId={supportedChainIds[2]}
-          />
-          {/* <TotalDepositPerNetwork chainId={5} title="Cumulative Volume"/>
+    return (
+        <Container maxWidth="xl">
+            <div className={classes.totalDepositContainer}>
+                <div className={classes.cumulativeDataRow}>
+                    <TotalDepositWithDuration
+                        chainIds={supportedChainIds}
+                        days={1}
+                        title="24 hr Volume"
+                        showComparision={true}
+                        lightBackground={true}
+                        style={durationStyle}
+                    />
+                    <CumulativeDeposit
+                        title="Total Volume"
+                        chainIds={supportedChainIds}
+                        style={durationStyle}
+                    />
+                    <UniqueWalletCount
+                        title="Unique User Count"
+                        chainIds={supportedChainIds}
+                        style={durationStyle}
+                    />
+                    <AverageTransferTime
+                        style={durationStyle}
+                        fromChainId={supportedChainIds[0]}
+                        toChainId={supportedChainIds[2]}
+                    />
+                    {/* <TotalDepositPerNetwork chainId={5} title="Cumulative Volume"/>
                     <TotalDepositPerNetwork chainId={80001} title="Cumulative Volume"/> */}
 
-          {/* <TotalDepositPerToken tokenSymbol="USDC" chainId={5}/>
+                    {/* <TotalDepositPerToken tokenSymbol="USDC" chainId={5}/>
                     <TotalDepositPerToken tokenSymbol="USDT" chainId={5}/>
                     <TotalDepositPerToken tokenSymbol="USDC" chainId={80001}/>
                     <TotalDepositPerToken tokenSymbol="USDT" chainId={80001}/> */}
-        </div>
+                </div>
 
-        {config.supportedChainArrray &&
-          config.supportedChainArrray.length > 0 &&
-          config.supportedChainArrray.map((item, index) => (
-            <div className={classes.chainDataRow} key={`NetworkRow_${index}`}>
-              <TotalDepositWithDuration
-                chainIds={[item.chainId]}
-                days={1}
-                title="24 hr Volume"
-                showComparision={true}
-              />
-              <CumulativeDeposit
-                title="Total Volume"
-                chainIds={[item.chainId]}
-              />
-              <UniqueWalletCount
-                title="Unique User Count"
-                chainIds={[item.chainId]}
-              />
+                {config.supportedChainArrray &&
+                    config.supportedChainArrray.length > 0 &&
+                    config.supportedChainArrray.map((item, index) => (
+                        <div
+                            className={classes.chainDataRow}
+                            key={`NetworkRow_${index}`}
+                        >
+                            <TotalDepositWithDuration
+                                chainIds={[item.chainId]}
+                                days={1}
+                                title="24 hr Volume"
+                                showComparision={true}
+                            />
+                            <CumulativeDeposit
+                                title="Total Volume"
+                                chainIds={[item.chainId]}
+                            />
+                            <UniqueWalletCount
+                                title="Unique User Count"
+                                chainIds={[item.chainId]}
+                            />
+                        </div>
+                    ))}
             </div>
-          ))}
-      </div>
-      <div className={classes.graphContainer}>
-        <div className={classes.graphComponentRow}>
-          <DailyDepositGraph chainIds={supportedChainIds} days={15} />
-          <FeeEarnedGraph chainIds={supportedChainIds} days={15} />
-        </div>
-        <div className={classes.graphComponentRow}>
-          {config.supportedChainArrray &&
-            config.supportedChainArrray.length > 0 &&
-            config.supportedChainArrray.map((item, index) => (
-              <AvailableLiquidity
-                chainId={item.chainId}
-                supportedTokenSymbols={["USDT", "USDC"]}
-                key={`AL_${index}`}
-              />
-            ))}
-        </div>
-        <div className={classes.graphComponentRow}>
-          {config.supportedChainArrray &&
-            config.supportedChainArrray.length > 0 &&
-            config.supportedChainArrray.map((item, index) => (
-              <AvailableLiquidity
-                chainId={item.chainId}
-                supportedTokenSymbols={["ETH"]}
-                key={`AL_${index}`}
-              />
-            ))}
-        </div>
-      </div>
-    </Container>
-  );
+            <div className={classes.graphContainer}>
+                <div className={classes.graphComponentRow}>
+                    <DailyDepositGraph chainIds={supportedChainIds} days={15} />
+                    <FeeEarnedGraph chainIds={supportedChainIds} days={15} />
+                </div>
+                <div className={classes.graphComponentRow}>
+                    {config.supportedChainArrray &&
+                        config.supportedChainArrray.length > 0 &&
+                        config.supportedChainArrray.map((item, index) => (
+                            <AvailableLiquidity
+                                chainId={item.chainId}
+                                supportedTokenSymbols={["USDT", "USDC"]}
+                                key={`AL_${index}`}
+                            />
+                        ))}
+                </div>
+                <div className={classes.graphComponentRow}>
+                    {config.supportedChainArrray &&
+                        config.supportedChainArrray.length > 0 &&
+                        config.supportedChainArrray.map((item, index) => (
+                            <AvailableLiquidity
+                                chainId={item.chainId}
+                                supportedTokenSymbols={["ETH"]}
+                                key={`AL_${index}`}
+                            />
+                        ))}
+                </div>
+            </div>
+        </Container>
+    );
 }
