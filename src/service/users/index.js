3b837a8e3152cab354559df94cd81383a3d960ea
Asheer Rizvi <asheer.vil@gmail.com>
2021-12-30 16:22:42 +0530

    Change query of total wallet counts

--- src/service/users/index.js
+++ src/service/users/index.js
@@ -1,40 +1,26 @@
-import { executeQuery } from '../subgraph';
-import { config } from '../../config';
+import { executeQuery } from "../subgraph";
+import { config } from "../../config";
 
 function getUniqueUserCountByChain(chainId, version) {
     return new Promise(async (resolve, reject) => {
         try {
-            let totalCount = 0;
-            if(!chainId) {
-                return reject("Invalid chainId passed as parameter. Please check the inputs");
+            if (!chainId) {
+                return reject(
+                    "Invalid chainId passed as parameter. Please check the inputs"
+                );
             }
-
-            let endOfData = false;
-            let skip = 0;
-            while(!endOfData) {
-
-                let query = `query {
-                    uniqueWallets(first: 1000, skip: ${skip}) {
-                        id
-                    }
-                }`
-                skip += 1000;
-                let data = await executeQuery(chainId, query, version);
-                if(data && data.data && data.data.uniqueWallets) {
-                    let aggregateData = data.data.uniqueWallets;
-                    if(aggregateData) {
-                        totalCount += aggregateData.length;
-                    }
-                    if(aggregateData.length == 0) {
-                        endOfData = true;
-                    }
-                } else {
-                    console.log(`No data returned from graph while getting unique user count for chain id ${chainId}`);
-                    endOfData = true;
+            const query = `{
+                uniqueWalletCounts{
+                  id,
+                  count
                 }
-            }
+            }`;
+            const {
+                data: { uniqueWalletCounts },
+            } = await executeQuery(chainId, query, version);
+            const { count: totalCount } = uniqueWalletCounts[uniqueWalletCounts.length - 1] || 0;
             resolve(totalCount);
-        } catch(error) {
+        } catch (error) {
             reject(error);
         }
     });
@@ -47,23 +33,23 @@ function getUniqueUserCount() {
             const fetchOptions = {
                 method: "GET",
                 headers: {
-                  'Content-Type': 'application/json;charset=utf-8'
-                }
-            }
-            let response = await fetch(`${config.hyphen.baseURL}${config.hyphen.getUniqueUserCountPath}`, fetchOptions);
-            if(response) {
+                    "Content-Type": "application/json;charset=utf-8",
+                },
+            };
+            let response = await fetch(
+                `${config.hyphen.baseURL}${config.hyphen.getUniqueUserCountPath}`,
+                fetchOptions
+            );
+            if (response) {
                 response = await response.json();
                 uniqueUserCount = response.uniqueUserCount;
             }
 
             resolve(uniqueUserCount);
-        } catch(error) {
+        } catch (error) {
             reject(error);
         }
     });
 }
 
-export {
-    getUniqueUserCount,
-    getUniqueUserCountByChain
-}
+export { getUniqueUserCount, getUniqueUserCountByChain };
